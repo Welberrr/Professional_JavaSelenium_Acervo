e233f2221a933652dcfda2126bcbf05771d63467
Welberrr <welberhenrique.costa@gmail.com>
2025-07-24 14:12:28 -0300

    Feat: Finalizando implementação geral de teste (Autor, Editora e livro)

--- src/test/java/testesSelenium/CamposTesteEditora.java
+++ src/test/java/testesSelenium/CamposTesteEditora.java
@@ -52,7 +52,7 @@ public class CamposTesteEditora {
 	}
 	
 	public void localizarBotaoExclusao() {
-		botaoDelete = driver.findElement(By.xpath("//*[@id=\"pn_id_334-table\"]/tbody/tr/td[3]/div[2]"));
+		botaoDelete = driver.findElement(By.xpath("//*[@id=\"pn_id_438-table\"]/tbody/tr/td[3]/div[2]"));
 	}
 	
 	public void localizarCampoConsulta() {
