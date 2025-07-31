c363f657670332fc7c1fba4c124abc28b155e731
Welberrr <welberhenrique.costa@gmail.com>
2025-07-31 14:51:41 -0300

    Feat: Implementação de cadastro de Classificação de gabinete e estruturação dos metodos

--- src/test/java/testesSelenium/CamposTesteClassificacaoGabinete.java
+++ src/test/java/testesSelenium/CamposTesteClassificacaoGabinete.java
@@ -1,5 +1,72 @@
 package testesSelenium;
 
-public class CamposTesteClassificacaoGabinete {
+import java.time.Duration;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CamposTesteClassificacaoGabinete {
+	private WebDriver driver;
+	private WebDriverWait wait;
+	
+	WebElement botaoMenu, botaoClassGabinete, botaoEngren, botaoGabinete, botaoAlterarGab, botaoNovaClass, campoDescricao, botaoGravar, botaoMenuFinal, botaoInicio;
+	
+	
+	public CamposTesteClassificacaoGabinete(WebDriver driver) {
+		this.driver = driver;
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+	}
+	
+	
+	public WebElement botaoEspera(String xpath) {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
+	}
+	
+	public void localizarBotaoMenu() {
+		botaoMenu = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]"));
+	}
+	
+	public void localizarBotaoClassGabinete() {
+		botaoClassGabinete = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/div[1]/app-sidebar/app-menu/ul/li[2]/ul/li[2]/a/span[1]"));
+	}
+	
+	public void localizarEngren() {
+		botaoEngren = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[1]/div/button"));
+	}
+	
+	public void LocalizarGabinete() {
+		botaoGabinete = driver.findElement(By.xpath("//*[@id=\"pn_id_3_0\"]/span[1]"));
+	}
+	
+	public void localizarBotaoAlterarGab() {
+		botaoAlterarGab = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[2]/div/p-dialog/div/div/div[3]/app-configuracao/form/div/div/button"));
+	}
+	
+	public void localizarBotaoNovaClass() {
+		botaoNovaClass = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/div[2]/div/ng-component/div/div/div/p-toolbar/div/div[2]/button[2]"));
+	}
+	
+	public void localizarCampoDescricao() {
+		campoDescricao = driver.findElement(By.xpath("//*[@id=\"descricao\"]"));
+	}
+	
+	public void localizarBotaoGravar() {
+		botaoGravar = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/div[2]/div/ng-component/div/div/form/div/div[2]/span[1]/button"));
+	}
+	
+	public void localizarBotaoMenuFinal() {
+		botaoMenuFinal = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]"));
+	}
+	
+	public void localizarBotaoInicio() {
+		botaoInicio = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/div[1]/app-sidebar/app-menu/ul/li[1]/ul/li/a"));
+	}
 }
--- src/test/java/testesSelenium/TesteClassificacaoGabinete.java
+++ src/test/java/testesSelenium/TesteClassificacaoGabinete.java
@@ -1,17 +1,97 @@
 package testesSelenium;
 
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class TesteClassificacaoGabinete {
 	
-	public void cadastrarClassificacaoGabinete() {
+	CamposTesteClassificacaoGabinete elemento;
+	private WebDriverWait wait;
+	
+	
+	public TesteClassificacaoGabinete(WebDriver driver) {
+		this.elemento = new CamposTesteClassificacaoGabinete(driver);
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+    }
+	
+	
+	public void cadastrarClassificacaoGabinete() throws InterruptedException {
+		elemento.botaoMenu = elemento.botaoEspera("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]");
+		elemento.getBotaoMenu().click();
+		Thread.sleep(1000);
 		
+		
+		elemento.localizarBotaoClassGabinete();
+		elemento.getBotaoClassGabinete();
+		Thread.sleep(1000);
+		
+		elemento.localizarEngren();
+		elemento.getBotaoEngren().click();
+		Thread.sleep(1000);
+		
+		elemento.LocalizarGabinete();
+		elemento.getBotaoGabinete().click();
+		Thread.sleep(1000);
+		
+		elemento.localizarBotaoAlterarGab();
+		elemento.getBotaoAlterarGab().click();
+		Thread.sleep(1000);
+		
+		elemento.localizarBotaoNovaClass();
+		elemento.getBotaoNovaClass().click();
+		Thread.sleep(1000);
+		
+		elemento.localizarCampoDescricao();
+		elemento.getCampoDescricao().sendKeys("?");
+		Thread.sleep(1000);
+		
+		elemento.localizarBotaoGravar();
+		elemento.getBotaoGravar().click();
+		Thread.sleep(1000);
+		
+		
+		elemento.localizarBotaoMenuFinal();
+		elemento.getBotaoMenuFinal().click();
+		Thread.sleep(1000);
+		
+		elemento.localizarBotaoInicio();
+		elemento.getBotaoInicio().click();
+		Thread.sleep(1000);
 	}
 	
-	public void editarClassificacaoGabinete() {
+	public void editarClassificacaoGabinete() throws InterruptedException {
+		elemento.botaoMenu = elemento.botaoEspera("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]");
+		elemento.getBotaoMenu().click();
+		Thread.sleep(1000);
 		
+		
+		
+		
+		elemento.localizarBotaoMenuFinal();
+		elemento.getBotaoMenuFinal().click();
+		Thread.sleep(1000);
+		
+		elemento.localizarBotaoInicio();
+		elemento.getBotaoInicio().click();
+		Thread.sleep(1000);
 	}
 	
-	public void excluirClassificacaoGabinite() {
+	public void excluirClassificacaoGabinite() throws InterruptedException {
+		elemento.botaoMenu = elemento.botaoEspera("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]");
+		elemento.getBotaoMenu().click();
+		Thread.sleep(1000);
+	
 		
+		
+		
+		elemento.localizarBotaoMenuFinal();
+		elemento.getBotaoMenuFinal().click();
+		Thread.sleep(1000);
+		
+		elemento.localizarBotaoInicio();
+		elemento.getBotaoInicio().click();
+		Thread.sleep(1000);
 	}
-
 }
