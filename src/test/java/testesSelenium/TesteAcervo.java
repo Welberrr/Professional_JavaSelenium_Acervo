5b11a099a9c2dab8d70abf8b3a22b58f0b2b180f
Welberrr <welberhenrique.costa@gmail.com>
2025-08-08 23:10:44 -0300

    Feat: Começando a implementação de cadastro Acervo

--- src/test/java/testesSelenium/TesteAcervo.java
+++ src/test/java/testesSelenium/TesteAcervo.java
@@ -1,24 +1,52 @@
 package testesSelenium;
 
-import org.openqa.selenium.WebDriver;
+import java.time.Duration;
 
-public class TesteAcervo {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+public class CamposTesteAcervo {
+
+	private WebDriver driver;
+	private WebDriverWait wait;
 	
-	private CamposTesteAcervo elemento;
+    WebElement botaoMenuInicio, botaoAcervo, botaoEngGabinete, botaoGabinete, botaoAlterar;
 	
-	public TesteAcervo(WebDriver driver) {
-		this.elemento = new CamposTesteAcervo(driver);
+	public CamposTesteAcervo(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(60));
     }
 	
-	public void cadastrarAcervo() {
-		
+	public WebElement botaoEspera(String xpath) {
+		return wait.until(ExpectedConditions.elementToBeClickable(By.xpath(xpath)));
 	}
 	
-	public void editarAcervo() {
-		
+	public void localizarBotaoMenuInicio() {
+		botaoMenuInicio = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]"));
+    }
+	
+	public void localizarBotaoAcervo() {
+		botaoAcervo = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/div[1]/app-sidebar/app-menu/ul/li[3]/ul/li[1]/a"));
 	}
 	
-	public void excluirAcervo() {
-		
+	public void localizarBotaoEngGabinete() {
+		botaoEngGabinete = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[1]/div/button"));
+	}
+	
+	public void localizarBotaoGabinete() {
+		botaoGabinete = driver.findElement(By.xpath("//*[@id=\"pn_id_3_0\"]"));
+	}
+	
+	public void localizarBotaoAlterar() {
+		botaoAlterar = driver.findElement(By.xpath("/html/body/app-root/app-layout/div/app-topbar/div[2]/div/p-dialog/div/div/div[3]/app-configuracao/form/div/div/button"));
 	}
 }
