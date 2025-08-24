df270e0fe45e446854ae032468654497eb955f62
Welberrr <welberhenrique.costa@gmail.com>
2025-08-24 17:49:51 -0300

    Fix: Tentativa de correção Relatorio Sintetico

--- src/test/java/testesSelenium/TesteRelatórios.java
+++ src/test/java/testesSelenium/TesteRelatórios.java
@@ -1,11 +1,11 @@
 package testesSelenium;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
-
-import br.jus.stf.acervo.model.dto.AutorDto;
-import br.jus.stf.acervo.model.dto.ClassificacaoLivroDto;
-import br.jus.stf.acervo.model.dto.LivroDto;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class TesteRelatorios {
 
@@ -69,7 +69,7 @@ public class TesteRelatorios {
 		
 		elemento.localizarBotaoPesquisar();
 		elemento.getBotaoPesquisar().click();
-		Thread.sleep(3000);
+		Thread.sleep(2000);
 		
 		elemento.localizarBotaoXLSX();
 		elemento.getBotaoXLSX().click();
@@ -86,47 +86,88 @@ public class TesteRelatorios {
 	}
 	
 	public void gerarRelatorioSintetico() throws InterruptedException {
-		elemento.botaoMenuInicio = elemento.botaoEspera("/html/body/app-root/app-layout/div/app-topbar/div[1]/button[1]");
+		// 1️⃣ Navegação pelos menus
 		elemento.getBotaoMenuInicio().click();
 		Thread.sleep(1000);
-		
+
 		elemento.localizarBotaoRelatorios();
 		elemento.getBotaoRelatorios().click();
 		Thread.sleep(1000);
-		
-		
+
 		elemento.localizarBotaoEngGab();
 		elemento.getBotaoEngGab().click();
 		Thread.sleep(1000);
-		
+
 		elemento.localizarBotaoGab();
 		elemento.getBotaoGab().click();
 		Thread.sleep(1000);
-		
+
 		elemento.localizarBotaoAlterarGab();
 		elemento.getBotaoAlterarGab().click();
 		Thread.sleep(1000);
-		
-		
+
+		// 1️⃣ Clica na aba "Relatório Sintético"
 		elemento.localizarBotaoRelatorioSintetico();
 		elemento.getBotaoRelatorioSintetico().click();
-		Thread.sleep(1000);
+		Thread.sleep(1000); // mantém a sua estratégia de sleep
+
+		// 2️⃣ Localiza o input dinamicamente
+		WebElement campo = null;
+		int tentativas = 0;
+		while (tentativas < 10) {
+		    try {
+		        campo = driver.findElement(By.id("classificacao"));
+		        if (campo.isDisplayed() && campo.getSize().getHeight() > 0 && campo.getSize().getWidth() > 0) {
+		            break;
+		        }
+		    } catch (Exception e) {
+		        // ignora
+		    }
+		    Thread.sleep(500);
+		    tentativas++;
+		}
+
+		if (campo == null) {
+		    throw new RuntimeException("Campo 'classificacao' não encontrado ou não interagível!");
+		}
+
+		// 3️⃣ Rola para o campo ficar visível
+		((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", campo);
+		Thread.sleep(200);
+
+		// 4️⃣ Digita "teste" e dispara os eventos Angular
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript(
+		    "arguments[0].value = 'teste';" +
+		    "arguments[0].dispatchEvent(new Event('input', { bubbles: true }));" +
+		    "arguments[0].dispatchEvent(new Event('change', { bubbles: true }));",
+		    campo
+		);
+
+		// 5️⃣ Pressiona Enter para pesquisar
+		Actions actions = new Actions(driver);
+		actions.moveToElement(campo).click().sendKeys(Keys.ENTER).perform();
+		Thread.sleep(500);
 		
-		elemento.localizarCampoClassSintetico();
-		elemento.getCampoClassSintetico().sendKeys("Teste", Keys.ENTER);
-		Thread.sleep(3000);
 		
+
+		// 5️⃣ Clica no botão XLSX do relatório sintetico
 		elemento.localizarBotaoXLSXSintetico();
 		elemento.getBotaoXLSXSintetico().click();
 		Thread.sleep(1000);
-		
-		
+
+		// 6️⃣ Fecha menus e retorna para a tela inicial
 		elemento.localizarBotaoMenuFinal();
 		elemento.getBotaoMenuFinal().click();
 		Thread.sleep(1000);
-		
+
 		elemento.localizarBotaoInicioFinal();
 		elemento.getBotaoInicioFinal().click();
 		Thread.sleep(1000);
+
+	}
+	
+	public void gerarEtiquetas() {
+		
 	}
 }
